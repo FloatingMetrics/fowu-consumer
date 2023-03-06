598f63492e71f4c6d2f72e5db567cb4a53c743b9
Therese <thestokkan@users.noreply.github.com>
2023-03-06 13:16:51 +0100

    Revert "Changed from handler to specific polling -> consumer works"

--- src/main/java/com/fowu/Consumer.java
+++ src/main/java/com/fowu/Consumer.java
@@ -1,46 +1,32 @@
 package com.fowu;
 
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 import io.vertx.kafka.client.consumer.KafkaConsumer;
-import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
-
 import java.time.Duration;
 import java.util.Properties;
 
 public class Consumer {
-  private final Properties props = PropertiesHelper.getProperties();
-  private final int TIME_OUT_MS = 1000;
-  private final int POLL_MS = 100;
   Vertx vertx = Vertx.vertx();
-  private final KafkaConsumer<String, JsonObject> consumer = KafkaConsumer.create(vertx, props);
-
+  private final Properties props = PropertiesHelper.getProperties();
+  private final KafkaConsumer<String, String> consumer = KafkaConsumer.create(vertx, props);
+  private final int TIME_OUT_MS = 5000;
 
   public Consumer() throws Exception {
   }
 
   public void run(String topicName) {
-    consumer.subscribe(topicName).onSuccess(v -> {
-      System.out.println("Consumer subscribed");
-
-      // Let's poll every second
-      vertx.setPeriodic(TIME_OUT_MS,
-                        timerId -> consumer.poll(Duration.ofMillis(POLL_MS)).onSuccess(records -> {
-                          for (int i = 0; i < records.size(); i++) {
-                            KafkaConsumerRecord<String, JsonObject> record = records.recordAt(i);
-                            System.out.println(
-                              "key=" + record.key() + ",value=" + record.value() + ",partition=" +
-                              record.partition() + ",offset=" + record.offset());
-                          }
-                        }).onFailure(cause -> {
-                          System.out.println(
-                            "Something went wrong when polling " + cause.toString());
-                          cause.printStackTrace();
-
-                          // Stop polling if something went wrong
-                          vertx.cancelTimer(timerId);
-                        }));
+    // Register a handler for handling incoming messages
+    consumer.handler(record -> {
+      System.out.println(
+        "Processing key=" + record.key() + ",value=" + record.value() + ",partition=" +
+        record.partition() + ",offset=" + record.offset());
     });
+
+    // Subscribe to topic
+    consumer
+      .subscribe(topicName)
+      .onSuccess(v -> System.out.println("subscribed"))
+      .onFailure(cause -> System.out.println("Could not subscribe " + cause.getMessage()));
 
   }
 }
