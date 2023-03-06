c9e4ba829e34f375448156bb280990ee2e35bb3b
Therese Stokkan <thestokkan@gmail.com>
2023-03-06 13:11:28 +0100

    Changed from handler to specific polling -> consumer works

--- src/main/java/com/fowu/ConsumerApplication.java
+++ src/main/java/com/fowu/ConsumerApplication.java
@@ -2,6 +2,6 @@ package com.fowu;
 
 public class ConsumerApplication {
   public static void main(String[] args) throws Exception {
-    new Consumer().run("test-topic");
+    new Consumer().run("weather");
   }
 }
