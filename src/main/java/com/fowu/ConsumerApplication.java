793f03d0524d90955b21a2e9b375ca5864b482e5
Therese Stokkan <thestokkan@gmail.com>
2023-03-07 13:27:24 +0100

    Refactored Consumer as verticle, removed redundand files

--- src/main/java/com/fowu/ConsumerApplication.java
+++ src/main/java/com/fowu/ConsumerApplication.java
@@ -2,6 +2,5 @@ package com.fowu;
 
 public class ConsumerApplication {
   public static void main(String[] args) throws Exception {
-    new Consumer().run("weather");
   }
 }
---
+++ src/main/java/com/fowu/ConsumerVerticle.java
@@ -0,0 +1,49 @@
+package com.fowu;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.kafka.client.consumer.KafkaConsumer;
+import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class ConsumerVerticle extends AbstractVerticle {
+  private final String topicName = "weather";
+  private final int TIME_OUT_MS = 1000;
+  private final int POLL_MS = 100;
+
+  @Override
+  public void start() throws Exception {
+    Properties props = PropertiesHelper.getProperties();
+    Vertx vertx = Vertx.vertx();
+    KafkaConsumer<String, JsonObject> consumer = KafkaConsumer.create(vertx, props);
+
+    consumer.subscribe(topicName).onSuccess(v -> {
+      System.out.println("Consumer subscribed");
+      poll(consumer);
+    });
+
+  }
+
+  private void poll(KafkaConsumer<String, JsonObject> consumer) {
+    // Let's poll every second
+    vertx.setPeriodic(TIME_OUT_MS,
+                      timerId -> consumer.poll(Duration.ofMillis(POLL_MS)).onSuccess(records -> {
+                        for (int i = 0; i < records.size(); i++) {
+                          KafkaConsumerRecord<String, JsonObject> record = records.recordAt(i);
+                          System.out.println(
+                            "key=" + record.key() + ",value=" + record.value() + ",partition=" +
+                            record.partition() + ",timestamp=" + record.timestamp() + ",offset=" + record.offset());
+                        }
+                      }).onFailure(cause -> {
+                        System.out.println(
+                          "Something went wrong when polling " + cause.toString());
+                        cause.printStackTrace();
+
+                        // Stop polling if something went wrong
+                        vertx.cancelTimer(timerId);
+                      }));
+  }
+}
