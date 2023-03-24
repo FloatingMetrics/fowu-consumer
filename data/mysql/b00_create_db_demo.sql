4fb0448b6e02d1370ec49307c97111a7d405ef71
alastair.crombie <alastair.crombie@accenture.com>
2023-03-24 15:45:17 +0100

    with create weather table in sql file

--- data/mysql/b00_create_db_demo.sql
+++ data/mysql/b00_create_db_demo.sql
@@ -1,3 +1,13 @@
 create database fowu;
 
 GRANT  CREATE, ALTER, DROP, SELECT, INSERT, UPDATE, DELETE ON fowu.* TO fowu_user;
+
+CREATE TABLE weather (
+                         time varchar(150),
+                         waveHeight double,
+                         wavePeriod double,
+                         waveDirection int,
+                         windSpeed double,
+                         windDirection int,
+                         PRIMARY KEY (time)
+);
