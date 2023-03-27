f43f35363b9adc19e942e277d1cfe0e896d0c0e4
alastair.crombie <alastair.crombie@accenture.com>
2023-03-27 15:58:35 +0200

    further database tweaks

--- src/main/java/com/fowu/PropertiesHelper.java
+++ src/main/java/com/fowu/PropertiesHelper.java
@@ -43,17 +43,11 @@ public class PropertiesHelper {
   public static JsonObject getDatasourceProperties() {
     JsonObject props = new JsonObject();
 
-    // props.put("url", "jdbc:mysql://mysql:3306/fowu");
-    // props.put("datasourceName", "fowu");
-    // props.put("username", "fowu_user");
-    // props.put("password", "1234");
-    // props.put("max_pool_size", 16);
-
-    props.put("url", "jdbc:sqlserver://fowu-database-server.database.windows.net:1433");
+    props.put("url", "jdbc:sqlserver://fowu.database.windows.net:1433");
     props.put("database", "fowu-database");
-    props.put("user", "fowu-server-access@fowu-database-server");
+    props.put("user", "fowu-server-access@fowu");
     props.put("password", "Therese1");
-    props.put("encrypt", "false");
+    props.put("encrypt", "true");
     props.put("trustServerCertificate", "false");
     props.put("hostNameInCertificate", "*.database.windows.net");
     props.put("loginTimeout", "30");
