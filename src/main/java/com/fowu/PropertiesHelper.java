23f2dbbe2d65d91c10b91c756bd7bcc63ba5acd4
Therese Stokkan <thestokkan@gmail.com>
2023-03-29 12:35:56 +0200

    Modified database properties url, transfer to db is working

--- src/main/java/com/fowu/PropertiesHelper.java
+++ src/main/java/com/fowu/PropertiesHelper.java
@@ -43,8 +43,8 @@ public class PropertiesHelper {
   public static JsonObject getDatasourceProperties() {
     JsonObject props = new JsonObject();
 
-    props.put("url", "jdbc:sqlserver://fowu.database.windows.net:1433");
-    props.put("database", "fowu-database");
+    props.put("url", "jdbc:sqlserver://fowu.database.windows.net:1433;databaseName=fowudatabase");
+    // props.put("database", "fowu-database");
     props.put("user", "fowu-server-access@fowu");
     props.put("password", "Therese1");
     props.put("encrypt", "true");
