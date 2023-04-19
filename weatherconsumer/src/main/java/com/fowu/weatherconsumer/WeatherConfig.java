aa0b778a01cd9daf25f7049fa271c894c3e4d7ef
Therese Stokkan <thestokkan@gmail.com>
2023-04-19 12:13:35 +0200

    Added custom WeatherConfig

---
+++ weatherconsumer/src/main/java/com/fowu/weatherconsumer/WeatherConfig.java
@@ -0,0 +1,17 @@
+package com.fowu.weatherconsumer;
+
+import com.fowu.common.PropertiesHelper;
+import java.util.Properties;
+
+public class WeatherConfig {
+  static Properties props;
+
+  public static Properties getWeatherProperties() throws Exception {
+    props = PropertiesHelper.getProperties();
+
+    props.setProperty("group.id", "weathergroup");
+    props.setProperty("default.topic", "weather");
+
+    return props;
+  }
+}
