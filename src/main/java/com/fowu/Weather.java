40747436bd51a67f7c3856484d7aae64cd053f29
Therese Stokkan <thestokkan@gmail.com>
2023-04-19 08:31:46 +0200

    Add captureTime to constructor, format indentation

--- src/main/java/com/fowu/Weather.java
+++ src/main/java/com/fowu/Weather.java
@@ -4,89 +4,62 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Weather {
+  String captureTime;
+  double waveHeight;
+  double wavePeriod;
+  int waveDirection;
+  double windSpeed;
+  int windDirection;
 
+  public Weather() {
+  }
 
-        String captureTime;
-        double waveHeight;
-        double wavePeriod;
-        int waveDirection;
-        double windSpeed;
-        int windDirection;
+  public Weather(String captureTime, double waveHeight, double wavePeriod,
+                 int waveDirection,
+                 double windSpeed, int windDirection) {
 
-        public Weather() {}
+    this.captureTime = captureTime;
+    this.waveHeight = waveHeight;
+    this.wavePeriod = wavePeriod;
+    this.waveDirection = waveDirection;
+    this.windSpeed = windSpeed;
+    this.windDirection = windDirection;
+  }
 
-        public Weather(double waveHeight, double wavePeriod, int waveDirection, double windSpeed, int windDirection) {
+  public String getCaptureTime() {
+    return captureTime;
+  }
 
-            LocalDateTime localDateTime = LocalDateTime.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss dd.MM.yyyy");
+  public double getWaveHeight() {
+    return waveHeight;
+  }
 
+  public double getWavePeriod() {
+    return wavePeriod;
+  }
 
-            this.captureTime = localDateTime.format(formatter);
-            this.waveHeight = waveHeight;
-            this.wavePeriod = wavePeriod;
-            this.waveDirection = waveDirection;
-            this.windSpeed = windSpeed;
-            this.windDirection = windDirection;
-        }
+  public int getWaveDirection() {
+    return waveDirection;
+  }
 
-        public String getCaptureTime() {
-            return captureTime;
-        }
+  public double getWindSpeed() {
+    return windSpeed;
+  }
 
-        public void setCaptureTime(String captureTime) {
-            this.captureTime = captureTime;
-        }
+  public int getWindDirection() {
+    return windDirection;
+  }
 
-        public double getWaveHeight() {
-            return waveHeight;
-        }
-
-        public void setWaveHeight(double waveHeight) {
-            this.waveHeight = waveHeight;
-        }
-
-        public double getWavePeriod() {
-            return wavePeriod;
-        }
-
-        public void setWavePeriod(double wavePeriod) {
-            this.wavePeriod = wavePeriod;
-        }
-
-        public int getWaveDirection() {
-            return waveDirection;
-        }
-
-        public void setWaveDirection(int waveDirection) {
-            this.waveDirection = waveDirection;
-        }
-
-        public double getWindSpeed() {
-            return windSpeed;
-        }
-
-        public void setWindSpeed(double windSpeed) {
-            this.windSpeed = windSpeed;
-        }
-
-        public int getWindDirection() {
-            return windDirection;
-        }
-
-        public void setWindDirection(int windDirection) {
-            this.windDirection = windDirection;
-        }
-
-        @Override
-        public String toString() {
-            return "Weather{" +
-                   "time=" + captureTime +
-                   ", waveHeight=" + waveHeight +
-                   ", wavePeriod=" + wavePeriod +
-                   ", waveDirection=" + waveDirection +
-                   ", windSpeed=" + windSpeed +
-                   ", windDirection=" + windDirection +
-                   '}';
-        }
+  @Override
+  public String toString() {
+    return "Weather{" +
+           "captureTime=" + captureTime +
+           ", waveHeight=" + waveHeight +
+           ", wavePeriod=" + wavePeriod +
+           ", waveDirection=" + waveDirection +
+           ", windSpeed=" + windSpeed +
+           ", windDirection=" + windDirection +
+           '}';
+  }
 
 }
