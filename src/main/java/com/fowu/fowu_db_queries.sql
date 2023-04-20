3e1e8625473d46656022313a3b02af34ed405280
Therese Stokkan <thestokkan@gmail.com>
2023-04-20 13:35:33 +0200

    SQL queries

--- src/main/java/com/fowu/fowu_db_queries.sql
+++ src/main/java/com/fowu/fowu_db_queries.sql
@@ -1,11 +1,18 @@
 USE fowudatabase;
 
--- DELETE FROM weather;
-SELECT *
-FROM weather;
+-- CREATE TABLE strain (
+--   captureTime VARCHAR(20) PRIMARY KEY NOT NULL,
+--   strain DOUBLE PRECISION
+-- );
 
 -- Change column data type (Done)
 -- ALTER TABLE weather ALTER COLUMN wavePeriod DOUBLE PRECISION;
 
+-- DELETE FROM weather;
+SELECT * FROM weather;
+-- SELECT * FROM strain;
+
+
 -- Describe table
-EXEC sp_help weather;
+-- EXEC sp_help weather;
+-- EXEC sp_help strain;
