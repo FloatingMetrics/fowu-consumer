ffac010c8e440b13eb433d2ebac3d4e49ec0d466
Therese Stokkan <thestokkan@gmail.com>
2023-04-21 15:41:54 +0200

    Consume strain data from four partitions and send to four tables

--- strainconsumer/src/main/java/com/fowu/strainconsumer/ConsumerVerticle.java
+++ strainconsumer/src/main/java/com/fowu/strainconsumer/ConsumerVerticle.java
@@ -9,10 +9,13 @@ import io.vertx.kafka.client.consumer.KafkaConsumerRecord;
 import io.vertx.sqlclient.Tuple;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class ConsumerVerticle extends AbstractVerticle {
   private final String topicName = "strain";
+  private final int NUMBER_OF_CONSUMERS = 4;
   private final int TIME_OUT_MS = 1000;
   private final int POLL_MS = 100;
 
@@ -21,22 +24,32 @@ public class ConsumerVerticle extends AbstractVerticle {
   public void start() throws Exception {
 
     Properties props = StrainConfig.getStrainProperties();
-    KafkaConsumer<String, JsonObject> consumer = KafkaConsumer.create(vertx, props);
+    List<KafkaConsumer<String, JsonObject>> consumers = new ArrayList<>();
 
-    consumer.subscribe(topicName)
-            .onSuccess(v -> {
-              System.out.println("Consumer subscribed to topic: " + topicName);
-              System.out.println("Custom properties: " + props);
-              poll(consumer);
-            })
-            .onFailure(cause -> System.err.println("Error cannot subscribe to topic: " + cause));
+    for (int i = 0; i < NUMBER_OF_CONSUMERS; i++) {
+      KafkaConsumer<String, JsonObject> consumer = KafkaConsumer.create(vertx, props);
+      consumers.add(consumer);
+    }
+
+    for (int i = 0; i < NUMBER_OF_CONSUMERS; i++) {
+      KafkaConsumer<String, JsonObject> consumer = consumers.get(i);
+      int consumerNum = i + 1;
+
+      consumer.subscribe(topicName)
+              .onSuccess(v -> {
+                System.out.println("Consumer subscribed to topic: " + topicName);
+                System.out.println("Custom properties: " + props);
+                poll(consumer, consumerNum);
+              })
+              .onFailure(cause -> System.err.println("Error cannot subscribe to topic: " + cause));
+    }
   }
 
   private Strain toParams(KafkaConsumerRecord<String, JsonObject> record) {
     return record.value().mapTo(Strain.class);
   }
 
-  private void poll(KafkaConsumer<String, JsonObject> consumer) {
+  private void poll(KafkaConsumer<String, JsonObject> consumer, int consumerNum) {
 
     vertx.setPeriodic(TIME_OUT_MS,
                       timerId -> consumer.poll(Duration.ofMillis(POLL_MS)).onSuccess(records -> {
@@ -50,7 +63,7 @@ public class ConsumerVerticle extends AbstractVerticle {
                           Strain strainData = toParams(record);
                           JsonObject datasourceConfig = PropertiesHelper.getDatasourceProperties();
                           JDBCPool pool = JDBCPool.pool(vertx, datasourceConfig);
-                          String query = "INSERT INTO strain (captureTime, strain) values (?, ?)";
+                          String query = "INSERT INTO strain" + consumerNum + " (captureTime, strain) values (?, ?)";
                           pool
                             .getConnection()
                             .onFailure(e -> {
